d44dd28dec20eb6d7d6ce51a3439b93ad27f4958
Charles Marks <clmarks15@gmail.com>
2019-01-07 08:20:37 -0500

    b4 factoring functions

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
     //handle input
     if(input === "list") {
-        console.log(todos);
+        console.log("**********")
+        todos.forEach(function(todo, i)    {
+            console.log(i + ": " + todo);
+        });
+        console.log("**********")
     } else if (input === "new") {
         //ask for new todo
         var newTodo = prompt("Enter new todo");
         //add to todos array
         todos.push(newTodo);
+        console.log("Added Todo");
+    } else if(input === "delete")   {
+        // ask for index of todo to be deleted
+        var index = prompt("Enter index of todo to delete");
+        //delete that todo 
+        // splice()
+        todos.splice(index, 1);
+        console.log("Deleted Todo");
     }
     //ask again for new input
     input = prompt("What would you like to do?");
